a45f62c99b78c07566a452266301285ca9df4fa7
Marwen Guidara <m.guidara@root.sa>
2018-03-01 09:25:07 +0100

    Add UserAuthService And EmployeeService

---
+++ RootEmployees/RootEmployees/Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using RootEmployees.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RootEmployees.Services
+{
+    public class EmployeeService
+    {
+        public List<Employee> EmplyeeList()
+        {
+            var EmployeeList = new List<Employee>
+            {
+                new Employee
+                {
+                    FullName = "Marwen Guidara",
+                    Email = "m.guidara@root.sa",
+                    PhoneNumber = "+21624422356",
+                    Departement = "Services"
+                },
+                new Employee
+                {
+                    FullName = "Mahdi Sassi",
+                    Email = "m.sassi@root.sa",
+                    PhoneNumber = "+216",
+                    Departement = "Services"
+                },
+                new Employee
+                {
+                    FullName = "Mohamed",
+                    Email = "m.elleuch@root.sa",
+                    PhoneNumber = "+216",
+                    Departement = "Backend"
+                },
+                new Employee
+                {
+                    FullName = "Salwa souissi",
+                    Email = "s.souissi@root.sa",
+                    PhoneNumber = "+216",
+                    Departement = "Frontend"
+                },
+                new Employee
+                {
+                    FullName = "Aiman Slamah",
+                    Email = "aiman@root.sa",
+                    PhoneNumber = "+21654338053",
+                    Departement = "Services"
+                }
+            };
+
+            return EmployeeList;
+        }
+    }
+}
--- RootEmployees/RootEmployees/Services/UserauthService.cs
+++ RootEmployees/RootEmployees/Services/UserauthService.cs
@@ -25,49 +25,5 @@ namespace RootEmployees.Services
 
             return UserAuthList;
         }
-
-        public List<Employee> EmplyeeList()
-        {
-            var EmployeeList = new List<Employee>
-            {
-                new Employee
-                {
-                    FullName = "Marwen Guidara",
-                    Email = "m.guidara@root.sa",
-                    PhoneNumber = "+21624422356",
-                    Departement = "Services"
-                },
-                new Employee
-                {
-                    FullName = "Mahdi Sassi",
-                    Email = "m.sassi@root.sa",
-                    PhoneNumber = "+216",
-                    Departement = "Services"
-                },
-                new Employee
-                {
-                    FullName = "Mohamed",
-                    Email = "m.elleuch@root.sa",
-                    PhoneNumber = "+216",
-                    Departement = "Backend"
-                },
-                new Employee
-                {
-                    FullName = "Salwa souissi",
-                    Email = "s.souissi@root.sa",
-                    PhoneNumber = "+216",
-                    Departement = "Frontend"
-                },
-                new Employee
-                {
-                    FullName = "Aiman Slamah",
-                    Email = "aiman@root.sa",
-                    PhoneNumber = "+21654338053",
-                    Departement = "Services"
-                }
-            };
-
-            return EmployeeList;
-        }
     }
 }
